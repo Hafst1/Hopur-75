01fb451e275ebdfdf7c8d79e8e7ddf4db4d31fcd
einars16 <einars16@ru.is>
2017-11-29 14:44:50 +0000

    comment

--- StoraVerkefnid/main.cpp
+++ StoraVerkefnid/main.cpp
@@ -6,13 +6,5 @@ int main()
 {
     cout << "Git er snilld" << endl;
 
-    cout << "Hello World!" << endl;
-    
-    cout << "test" << endl;
-    
-    cout << "nytt test" << endl;
-    
-    cout << "testtest" << endl;
-
     return 0;
 }
