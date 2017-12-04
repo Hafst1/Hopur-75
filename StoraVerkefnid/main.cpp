2cbb3ffe8324add944bc2b8f0a063cad40418d69
Hafsteinn <hafsteinn24@gmail.com>
2017-12-04 13:47:16 +0000

    update

--- StoraVerkefnid/main.cpp
+++ StoraVerkefnid/main.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
 #include <fstream>
-#include "MainUI.h"
 
 using namespace std;
 
 int main()
 {
-    MainUI mainui;
 
-    mainui.startUI();
-
+    cout << "hello world!" << endl;
 
     return 0;
 }
